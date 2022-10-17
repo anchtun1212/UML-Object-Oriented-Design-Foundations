4f91148f74a3bcffd142b6a7e2dfe2e699c92877
Mohammed Ayman <m.ayman@tamkeentech.sa>
2022-10-17 14:14:46 +0300

    first commit

---
+++ src/main/java/com/anchtun/ood/OodApplication.java
@@ -0,0 +1,13 @@
+package com.anchtun.ood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anchtun/ood/OodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchtun.ood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
